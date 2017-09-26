f36e6606f0b6c0ba9d4eed5eebe20543ed16e15e
Eivind Samuelsen <eivind.samuelsen@gmail.com>
2017-09-26 08:44:36 +0200

    Added main.cpp, made solution compile and open a window

---
+++ structural/main.cpp
@@ -0,0 +1,11 @@
+#include <oui_window.h>
+
+oui::window::Description oui::window::initialize()
+{
+	return { "structural", 1280, 720 };
+}
+
+void oui::window::update(oui::Rectangle area, oui::Input& input)
+{
+
+}
